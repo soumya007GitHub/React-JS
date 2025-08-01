f8c18d7d6a03db9e2ca5cb8e4a7e58491dd73e65
soumya007GitHub <130821564+soumya007GitHub@users.noreply.github.com>
2025-08-01 23:29:34 +0530

    todo task completed

--- 11-ToDo-App/my-app/src/App.jsx
+++ 11-ToDo-App/my-app/src/App.jsx
@@ -4,6 +4,7 @@ import DisplayToDo from "./components/DisplayToDo";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [toggleButton, setToggleButton] = useState(false);
 
   useEffect(() => {
     const stored = localStorage.getItem("data");
@@ -14,20 +15,27 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem("data", JSON.stringify(todoList));
-  }, [todoList]);
+}, [todoList]);
+
+const markComplete = (id) => {
+    const updatedList = todoList.map(item =>
+      item.id === id ? { ...item, completed: true } : item
+    );
+    setTodoList(updatedList);
+  };
 
   return (
     <div className="body w-screen h-full flex flex-col justify-center items-center">
-      <div className="container w-7/10 border-1 border-red-500 p-4 flex flex-col">
+      <div className="container w-7/10 border-1 border-slate-500 rounded-md p-4 flex flex-col">
         <InputField setTodoList={setTodoList} todoList={todoList} />
         <div className="headings flex justify-between border-b-1 pb-2 border-slate-500">
           <h3>All Todos</h3>
           <div className="showCompleted">
-            <input type="checkbox" name="completedTodos" id="completedTodos" className="mx-1" />
+            <input type="checkbox" name="completedTodos" id="completedTodos" className="mx-1" value={toggleButton} onClick={()=>setToggleButton(!toggleButton)}/>
             <label htmlFor="completedTodos">Show Completed</label>
           </div>
         </div>
-        <DisplayToDo todoList={todoList} />
+        <DisplayToDo todoList={todoList} markComplete={markComplete} showCompleted={toggleButton}/>
       </div>
     </div>
   );
--- 11-ToDo-App/my-app/src/components/DisplayToDo.jsx
+++ 11-ToDo-App/my-app/src/components/DisplayToDo.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-function DisplayToDo({ todoList }) {
+function DisplayToDo({ todoList, markComplete, showCompleted }) {
   return (
     <div>
       <h2 className="text-lg font-bold mb-2">Todo List:</h2>
       <ul>
-        {todoList.map((item) => (
-          <li key={item.id} className="mb-2 p-2 bg-violet-900 rounded text-white">
-            {item.value}
-          </li>
-        ))}
+        {todoList
+          .filter(item => showCompleted ? true : !item.completed)
+          .map((item) => (
+            <li
+              key={item.id}
+              className={`mb-2 p-2 px-5 rounded bg-blue-800 text-white flex justify-between items-center ${item.completed ? 'line-through' : ''
+                }`}
+            >
+              {item.value}
+              <button className=' hover:text-red-500' onClick={() => markComplete(item.id)}>
+                <i className={`fa-solid fa-check ${item.completed ? 'text-green-500': 'text-red-500' }`}></i>
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
